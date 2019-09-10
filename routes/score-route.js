15a385de5cdb15effe619725fbf5e86caf5ac1d0
Suryansh Sugandhi <suryansh71199@gmail.com>
2019-09-10 14:38:03 +0530

    Score route added

--- routes/score-route.js
+++ routes/score-route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const User = require('../models/user')
 
-router.get('/', isloggedIn, (req, res)=>{
+router.get('/', isLoggedIn, (req, res)=>{
     let user = req.user;
     let score = 0;
 

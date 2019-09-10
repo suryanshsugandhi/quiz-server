f2f75c80c354935ca58f0e2a91ca167a65363290
Suryansh Sugandhi <suryansh71199@gmail.com>
2019-09-10 16:05:28 +0530

    Score route added

--- routes/score-route.js
+++ routes/score-route.js
@@ -5,29 +5,15 @@ const User = require('../models/user')
 
 router.get('/', isLoggedIn, (req, res)=>{
     let user = req.user;
-    let score = 0;
 
     User.findById(req.user._id, (err, user)=>{
         if(err){
             console.log("Score fetching error>>>", user.email);
         }
-        let userAnswers = [];
-        let correctAnswers = [];
-
-        user.questions.forEach(element => {
-            correctAnswers.push(element.answer);
-        });
-        user.answers.forEach(element => {
-            userAnswers.push(element.option);
-        });
-
-        for(let i = 0; i < userAnswers.length; i ++){
-            if(userAnswers[i] == correctAnswers[i])
-                ++score;
-        }
+        var score = await getScore(user);
     })
     .then((reqUser)=>{
-        User.findByIdAndUpdate(req.user._id, {score: score}, (err, user)=>{
+        User.findByIdAndUpdate(reqUser._id, {score: score}, (err, user)=>{
             if(err)
                 console.log("Error updating score>>>", user.email, score);
         })
@@ -36,6 +22,26 @@ router.get('/', isLoggedIn, (req, res)=>{
         })
     })
 })
+
+async function getScore(user){
+    let score = 0;
+    let userAnswers = [];
+    let correctAnswers = [];
+
+    user.questions.forEach(element => {
+        correctAnswers.push(element.answer);
+    });
+    user.answers.forEach(element => {
+        userAnswers.push(element.option);
+    });
+
+    for(let i = 0; i < userAnswers.length; i ++){
+        if(userAnswers[i] == correctAnswers[i])
+            ++score;
+    }
+
+    return score
+}
 
 
 function isLoggedIn(req, res, next){
